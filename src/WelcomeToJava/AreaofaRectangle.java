70d005c450d8344c0684921afe0effe1e063f94b
calebm01 <42648514+calebm01@users.noreply.github.com>
2018-09-18 12:26:56 -0600

    Add files via upload

---
+++ src/WelcomeToJava/AreaofaRectangle.java
@@ -0,0 +1,16 @@
+package WelcomeToJava;
+/*
+ * Caleb Mouritsen
+ * 9/10/18
+ * Finding the area and perimeter of a rectangle
+ */
+public class AreaofaRectangle {
+
+	public static void main(String[] args) {
+		//Finding the perimeter of a rectangle
+			System.out.println(4.5*2 + 7.9*2);
+		//Finding the area of a rectangle
+			System.out.println(4.5*7.9);
+	}
+
+}
